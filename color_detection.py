72e64fdbdbe4748953490ab67183fb01dc6bd8dd
nschiermeier <nschierm@purdue.edu>
2023-07-09 18:05:45 -0700

    updated to separate training and detection

--- color_detection.py
+++ color_detection.py
@@ -5,17 +5,23 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-colors_data = pd.read_csv('data/first 50 colors rgb.csv')
-print(colors_data)
-X = colors_data.drop(columns = ['Name', 'Broad Color'])
-y = colors_data['Broad Color']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-model = DecisionTreeClassifier()
-model.fit(X_train, y_train)
-predictions = model.predict(X_test)
+def train_model():
+  colors_data = pd.read_csv('data/RGB_labeled_colors.csv')
 
-score = accuracy_score(y_test, predictions)
-score
-print(score)
-print(model.predict([[10, 200, 200]]))
+  X = colors_data.drop(columns = ['Name', 'Broad Color']) # input
+  y = colors_data['Broad Color']                          # output
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+  model = DecisionTreeClassifier()
+  model.fit(X_train.values, y_train)
+  predictions = model.predict(X_test.values)
+
+  score = accuracy_score(y_test, predictions)
+  # print(score)
+  return model
+
+# This needs to be expanded and more colors need to be added
+def detect_color(model, color_array):
+    # print(model.predict([[50, 168, 82]]))
+  return model.predict([color_array])
